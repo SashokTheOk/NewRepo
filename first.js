c84f5c48ba4a36a0cb0493032f06c0897289ca9f
SashOK <qa.shvets@gmail.com>
2025-06-16 19:51:00 +0300

    something new about error handling

--- first.js
+++
@@ -1,4 +0,0 @@
-function rectangle(a, b) {
-  return a * b;
-} 
-module.exports = rectangle;
--- first.test.js
+++ first.test.js
@@ -1,26 +1,8 @@
-const rectangle = require('./rectangle');
-describe('RectangleArea', () => { 
-    test('should return 20 for length 5 and width 4', () => {
-        expect(rectangle.area(5, 4)).toBe(20);
-    }
-    );
-    test (`trhrows an error for negative length`, () => {
-        expect(() => rectangle.area(-5, 4)).toThrow('Length and width must be positive numbers');
-    }   
-    );
-    test (`trhrows an error for negative width`, () => {
-        expect(() => rectangle.area(5, -4)).toThrow('Length and width must be positive numbers');
-    }   
-    );  
-    test (`trhrows an error for zero length`, () => {
-        expect(() => rectangle.area(0, 4)).toThrow('Length and width must be positive numbers');
-    }   
-    );  
-    test (`trhrows an error for zero width`, () => {
-        expect(() => rectangle.area(5, 0)).toThrow('Length and width must be positive numbers');
-    }   
-    );
-    test('should return 0 for length 0 and width 0', () => {
-        expect(rectangle.area(0, 0)).toBe(0);
-    });
-})
+const axios = require('axios');
+
+test ("get incorrect URL", async () => {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/incorrect-url')
+}
+)
+console.log(response.data);
+expect(response.status).toBe(404);
